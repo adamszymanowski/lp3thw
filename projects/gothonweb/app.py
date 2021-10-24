39bebf00637844ca2af83b32efeb8c0d163249b5
Adam Szymanowski <adam.szym@yahoo.pl>
2021-10-24 18:25:08 +0200

    Exercise 51 - Getting Input from a Browser

--- projects/gothonweb/app.py
+++ projects/gothonweb/app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/hello', methods=['POST', 'GET'])
+def index():
     greeting = "Hello World"
-    return render_template("index.html", greeting=greeting)
+
+    if request.method == "POST":
+        print("post")
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet}, {name}"
+        return render_template("index.html", greeting=greeting)
+    else:
+        return render_template("hello_form.html")
+
 
 if __name__ == "__main__":
     app.run()
